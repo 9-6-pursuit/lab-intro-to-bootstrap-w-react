2d60e88de9cdcf04735738f5e12dda0268b986ad
Lakisha J <lakishajohnson@pursuit.org>
2023-05-05 12:15:51 -0400

    Added footer to page

---
+++ src/components.js/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="p-3 text-center" style={{ backgroundColor: "gold" }} >
+      <p> Copyright <b>MyTravelBlog</b> 2020</p>
+      <a className='text-dark' href='https://mdbootstrap.com/'>
+          Contact me
+        </a>
+    </footer>
+  );
+}
+
+export default Footer;
