167898546ab57af19121dfa847458942588a7e12
Lakisha J <lakishajohnson@pursuit.org>
2023-05-05 16:10:42 -0400

    Added links in footer

--- src/components.js/Footer.js
+++ src/components.js/Footer.js
@@ -4,9 +4,14 @@ function Footer() {
   return (
     <footer className="p-3 text-center" style={{ backgroundColor: "gold" }} >
       <p> Copyright <b>MyTravelBlog</b> 2020</p>
-      <a className='text-dark' href='https://mdbootstrap.com/'>
+      <a className='text-dark' href='https://www.linkedin.com/in/lakisha-johnson-0b0587219/'>
           Contact me
         </a>
+        <div>
+        <a className='text-dark' href='https://github.com/lakishaJohnson'>
+          About the author
+        </a>
+        </div>
     </footer>
   );
 }
