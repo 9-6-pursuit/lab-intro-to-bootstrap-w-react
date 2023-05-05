a22d1a82862ba03c63b2f68afb486d50b882426e
Lakisha J <lakishajohnson@pursuit.org>
2023-05-05 12:13:12 -0400

    Swapped classNames to make it look more like the example

--- src/components.js/SearchBar.js
+++ src/components.js/SearchBar.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 export default function SearchBar() {
-
-    
   return (
-    <div className="input-group mb-3">
-      <label htmlFor="searches" className="form-label">
+    <div className="input-group mb-3 search-box">
+      <label htmlFor="searches" className="form-label me-3">
         Search posts by location...
       </label>
-      <input type="text" className="form-control" aria-label="Search" />
-      <button className="btn btn-warning" type="button">
-        Cancel
+      <input type="text" className="form-control me-2" aria-label="Search" />
+      <button className="btn btn-warning me-5  search-btn" type="button">
+        Search
       </button>
     </div>
-  );
+  )
+    /*<div className="container">
+      <div className="d-flex">
+        <label for="SearchBar">Search posts by location</label>
+        <input type="text" className="form-control mr-1" />
+        <button className="btn btn-warning search-btn" type="button">
+          Search
+        </button>
+      </div>
+    </div>
+  );*/
 }
