0959c3d1719b95a62f117eb13001de97b2770f5a
Lakisha J <lakishajohnson@pursuit.org>
2023-05-05 19:22:35 -0400

    Added handleSearch, handleChange, and handleClick, for search button onClick

--- src/components.js/SearchBar.js
+++ src/components.js/SearchBar.js
@@ -1,25 +1,50 @@
 import React from "react";
+import { useState } from "react";
 
-export default function SearchBar() {
+export default function SearchBar({ posts, setPosts }) {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (search) => {
+    const filteredPosts = posts.filter((post) => {
+      const titleMatch = post.title
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const locationMatch = post.location
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return titleMatch || locationMatch;
+    });
+    setPosts(filteredPosts);
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    handleSearch(search);
+  };
+
   return (
     <div className="input-group mb-3 search-box">
-      <label htmlFor="searches" className="form-label me-3">
+      <label htmlFor="searches" className="form-label me-5">
         Search posts by location...
       </label>
-      <input type="text" className="form-control me-2" aria-label="Search" />
-      <button className="btn btn-warning me-5  search-btn" type="button">
+      <input
+        type="text"
+        className="form-control me-2"
+        aria-label="Search"
+        value={search}
+        onChange={handleChange}
+      />
+      <button
+        onClick={handleClick}
+        className="btn btn-warning me-5  search-btn"
+        type="button"
+      >
         Search
       </button>
     </div>
-  )
-    /*<div className="container">
-      <div className="d-flex">
-        <label for="SearchBar">Search posts by location</label>
-        <input type="text" className="form-control mr-1" />
-        <button className="btn btn-warning search-btn" type="button">
-          Search
-        </button>
-      </div>
-    </div>
-  );*/
+  );
 }
