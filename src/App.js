0b5ea2a9b7feb1b6e245483d44941f503288d944
Lakisha J <lakishajohnson@pursuit.org>
2023-05-05 19:23:39 -0400

    Added setPosts property

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import Navbar from "./components.js/Navbar";
 import SearchBar from "./components.js/SearchBar";
 import Posts from "./components.js/Posts";
 import Table from "./components.js/Table";
+import Footer from "./components.js/Footer";
 
 function App() {
   const [posts, setPosts] = useState(postData);
-  // console.log(postData);
   // const [search, setSearch] = useState("");
+  
+    // const handleSearch = () => {
+    //   const filteredPosts = posts.filter((post) => {
+    //     const titleMatch = post.title.toLowerCase().includes(search.toLowerCase());
+    //     const locationMatch = post.location.toLowerCase().includes(search.toLowerCase());
+    //     return titleMatch || locationMatch;
+    //   });
+    //   setPosts(filteredPosts);
+    // }; 
 
-  // const handleSearch = (event) => {
-  //   setSearch(event.target.value);
-  // };
-  
-  
   return (
     <main>
       <Navbar />
       <Dquote />
-      <SearchBar />
+      <SearchBar posts={posts} setPosts={setPosts}/>
       <div className="container">
         <div className="row">
           <div className="col-md-8">
@@ -31,6 +35,7 @@ function App() {
           </div>
         </div>
       </div>
+      <Footer />
     </main>
   );
 }
