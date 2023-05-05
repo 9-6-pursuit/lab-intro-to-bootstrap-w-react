e2ebfcf374906cbbe86c91b7cf1c8d84c8d987e1
Lakisha J <lakishajohnson@pursuit.org>
2023-05-05 12:09:05 -0400

    Updated to abbreviate post content

--- src/components.js/Posts.js
+++ src/components.js/Posts.js
@@ -19,11 +19,11 @@ function Posts({ posts }) {
   const PostCard = ({ title, location, content, photoUrl }) => {
     return (
       <div className="card mb-3">
-        <img src={photoUrl} className="card-img-top card-img-fix" alt={title} />
+        <img src={photoUrl} className="card-img-top card-img-fixed" alt={title} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{location}</h6>
-          <p className="card-text">{content.substring(0, 100)}...</p>
+          <p className="card-text">{content.substring(0, 90)}...</p>
           <button className="btn btn-warning">Go to post ▶️</button>
         </div>
       </div>
