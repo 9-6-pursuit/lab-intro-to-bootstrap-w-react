576e37198bc30b24800c04e5e60989a3df17f570
Lakisha J <lakishajohnson@pursuit.org>
2023-05-06 16:48:36 -0400

    Reset search bar

--- src/components.js/SearchBar.js
+++ src/components.js/SearchBar.js
@@ -24,6 +24,7 @@ export default function SearchBar({ posts, setPosts }) {
   const handleClick = (event) => {
     event.preventDefault();
     handleSearch(search);
+    setSearch("")
   };
 
   return (
