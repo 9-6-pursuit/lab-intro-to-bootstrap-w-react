30ffee93a04a5a84ebaa7f22ed52542e599eea24
Lakisha J <lakishajohnson@pursuit.org>
2023-05-06 14:52:15 -0400

    Added href attribute to links

--- src/components.js/Navbar.js
+++ src/components.js/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand display-4 p-3"><strong>
+        <a href="https://example.com"className="navbar-brand display-4 p-3"><strong>
           Travel{" "}</strong>
           <span
             style={{
@@ -17,12 +17,12 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page">
+              <a href="https://example.com"className="nav-link active" aria-current="page">
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link">Popular posts</a>
+              <a href="https://example.com" className="nav-link">Popular posts</a>
             </li>
           </ul>
         </div>
