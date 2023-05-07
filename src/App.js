5a43d6488f83493c6507558f641804fbd015b104
Lakisha J <lakishajohnson@pursuit.org>
2023-05-07 12:10:15 -0400

    Updated App.js for error message

--- src/App.js
+++ src/App.js
@@ -8,17 +8,50 @@ import Table from "./components.js/Table";
 import Footer from "./components.js/Footer";
 
 function App() {
-  const [posts, setPosts] = useState(postData); 
+  const [posts, setPosts] = useState(postData);
+  const [noResults, setNoResults] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = () => {
+    const filteredPosts = posts.filter((post) => {
+      const titleMatch = post.title
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const locationMatch = post.location
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return titleMatch || locationMatch;
+    });
+    if (filteredPosts.length === 0) {
+      setNoResults(true);
+    } else {
+      setNoResults(false);
+      setPosts(filteredPosts);
+    }
+  };
 
   return (
     <main>
       <Navbar />
       <Dquote />
-      <SearchBar posts={posts} setPosts={setPosts}/>
+      <SearchBar
+        posts={posts}
+        setPosts={setPosts}
+        handleSearch={handleSearch}
+        search={search}
+        setSearch={setSearch}
+      />
       <div className="container">
         <div className="row">
           <div className="col-md-8">
-            <Posts posts={posts} />
+            {noResults ? (
+              <div className="alert alert-danger">
+                No posts found with the search term "{search}". Please try
+                again.
+              </div>
+            ) : (
+              <Posts posts={posts} />
+            )}
           </div>
           <div className="col-md-4">
             <Table posts={posts} />
