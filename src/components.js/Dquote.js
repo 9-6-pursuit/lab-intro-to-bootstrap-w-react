0e3491cb104051fd7d541a2ae67b46d0599a7a6a
Lakisha J <lakishajohnson@pursuit.org>
2023-05-07 11:11:40 -0400

    Changed hero font

--- src/components.js/Dquote.js
+++ src/components.js/Dquote.js
@@ -6,10 +6,7 @@ export default function Dquote() {
       className="container-fluid d-flex justify-content-center align-items-center my p-4"
       style={{ backgroundColor: "#f2f2f2", border: "2px solid #ccc" }}
     >
-      <h2
-        className="p-4"
-        style={{ fontFamily: "cursive", textAlign: "center" }}
-      >
+      <h2 className="p-4 script-font">
         Travel is the only thing you buy that makes you richer...
       </h2>
     </div>
