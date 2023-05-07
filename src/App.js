c9964d5e3777d792e6303ede92810a3cb240c6fe
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-05-07 10:49:32 -0400

    finished Nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import postData from "./data/posts.json";
 
+import Nav from "./component/Nav";
+
 function App() {
   console.log(postData);
-  return <main></main>;
+  return (
+    <main>
+      <Nav />
+    </main>
+  );
 }
 
 export default App;
