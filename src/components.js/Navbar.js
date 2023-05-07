f8e99b0a4e22bd5514f7702831cf2fbd987bf962
Lakisha J <lakishajohnson@pursuit.org>
2023-05-07 12:25:26 -0400

    Looking more like the example

--- src/components.js/Navbar.js
+++ src/components.js/Navbar.js
@@ -2,13 +2,18 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a href="https://example.com"className="navbar-brand display-4 p-3"><strong>
-          Travel{" "}</strong>
+        <a
+          href="https://example.com"
+          className="navbar-brand display-4 p-3"
+          style={{ fontSize: 30 }}
+        >
+          <strong>Travel </strong>
           <span
+            className="script-font"
             style={{
               color: "gold",
-              fontFamily: "cursive",
               fontWeight: "bold",
+              fontSize: 30,
             }}
           >
             Blog
@@ -17,12 +22,18 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a href="https://example.com"className="nav-link active" aria-current="page">
+              <a
+                href="https://example.com"
+                className="nav-link active"
+                aria-current="page"
+              >
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a href="https://example.com" className="nav-link">Popular posts</a>
+              <a href="https://example.com" className="nav-link">
+                Popular posts
+              </a>
             </li>
           </ul>
         </div>
