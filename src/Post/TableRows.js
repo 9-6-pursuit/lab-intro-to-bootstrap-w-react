b2606c042fe03c238bc14a82f931da6e053b9c15
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-05-07 13:59:55 -0400

    almost ready to submit

--- src/Post/TableRows.js
+++ src/Post/TableRows.js
@@ -3,10 +3,14 @@ import React from "react";
 function TableRows({ locations }) {
   return (
     <>
-      {locations.map(location => (
+      {locations.map((location) => (
         <tr key={location.name}>
           <td>{location.name}</td>
-          <td>{location.count}</td>
+          <td className="text-center">
+            <span className="rounded-pill bg-secondary text-white px-2">
+              {location.count}
+            </span>
+          </td>
         </tr>
       ))}
     </>
