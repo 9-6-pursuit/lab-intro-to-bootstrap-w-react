8acf871623f66689a59b1f773d3fb54fdef1da8d
corvettejames <corvettejames@icloud.com>
2023-05-07 13:04:24 -0400

    I need more help

---
+++ src/data/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='container-fluid text-end bg-warning p-4'>
+      <div className='row'>
+        <div className="text-center">
+          <p>Contact me </p>     
+          <p>About the Author </p> 
+        </div>
+      </div>
+     Copyright <span><b>MyTravelblog</b></span> 2020
+    </footer>
+  )
+}
+
+export default Footer
