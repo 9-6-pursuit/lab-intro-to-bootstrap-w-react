02f6cfcd53ec515fb7d8de980e826f19197e63eb
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-05-08 18:10:59 -0400

    done I think ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import postData from "./data/posts.json";
+import images from "./images";
+import postData from "./data/posts.json"
+import Header from "./Components/Header";
+import Quote from "./Components/Quote";
+import Search from "./Components/Search";
+import Posts from "./Components/Posts";
+import Table from "./Components/Table";
+import Footer from "./Components/Footer";
+
+import { useState } from "react";
 
 function App() {
-  console.log(postData);
-  return <main></main>;
-}
+  const [posts, setPosts] = useState(postData);
+  const [search, setSearch] = useState("");
 
+  function handleSearch(event) {
+    // console.log(event.target.value.toLowerCase());
+    setSearch(event.target.value);
+    let filtered = postData.filter(post => post.location.toLowerCase().includes(event.target.value.toLowerCase()));
+    // console.log(filteredPosts)
+    setPosts(filtered);
+  }
+
+  return (
+    <main>
+      <Header/>
+      <Quote/>
+      <section className="container">
+      <Search
+          postData={postData}
+          setPosts={setPosts}
+          handleSearch={handleSearch}
+        />
+        <section className="row row-gap-4">
+        <Posts
+            posts={posts}
+            search={search}
+          />
+          <Table
+            posts={postData}
+          />
+        </section>
+      </section>
+      <Footer/>
+    </main>
+  );
+}
 export default App;
